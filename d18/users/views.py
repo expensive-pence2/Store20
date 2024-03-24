4441d9295a6fd8160e40eeb8a7ddc19c198cb568
ddfxfxfc <fetodfkjghjdhgjjdhsjejldahjckhjkcahxcjhxjk>
2024-03-24 17:27:59 +1000

    d23

--- d18/users/views.py
+++ d18/users/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.urls import reverse
+from d18.users.models import User
+from d18.users.forms import UserLoginForm
+from django.shortcuts import render, HttpResponseRedirect
 
-app_name = 'users'
-# Create your views here.
+from django.contrib import auth
+
+def Login(request) :
+    if request.method == "POST":
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            username = request.POST["usernane"]
+            password = request.POST["password"]
+            user = auth.authenticate(usernane = username, password = password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    else:
+        form = UserLoginForm()
+    context = {
+        'form' :form,
+    }
+
+    return render(request, 'users/login.html', context = context)
+
+
+def register (request) :
+       return render(request, 'users/register.html')
