458e58ad056d3574f0b57962a94060ed21fdaf8c
ddfxfxfc <fetodfkjghjdhgjjdhsjejldahjckhjkcahxcjhxjk>
2024-03-24 17:27:59 +1000

    d23

--- d18/users/urls.py
+++ d18/users/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+
+from .views import *
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', login, name = 'login'),
+    path('register/', register, name = 'register'),
+    path('profile/', profile, name = 'profile')
+]
--- d18/users/views.py
+++ d18/users/views.py
@@ -1,4 +1,53 @@
-from django.shortcuts import render
+from django.urls import reverse
+from d18.users.models import User
+from d18.users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.shortcuts import render, HttpResponseRedirect
 
-app_name = 'users'
-# Create your views here.
+from django.contrib import auth
+
+def login(request) :
+    if request.method == "POST":
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(usernane = username, password = password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    else:
+        form = UserLoginForm()
+    context = {
+        'form' :form,
+    }
+
+    return render(request, 'users/login.html', context = context)
+
+
+def register (request) :
+    if request.method == 'POST':
+        form = UserRegistrationForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form':form,}
+
+
+    return render(request, 'users/register.html', context = context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance = request.user, data = request.POST, files = request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance = request.user)
+        context = {'Title':'Профиль', 'form':form,}
+        return render(request, 'users/profile.html', context = context)
