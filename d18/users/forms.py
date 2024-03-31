a17d938cd24ca293d6b6d7129491685a4f74ab14
ddfxfxfc <fetodfkjghjdhgjjdhsjejldahjckhjkcahxcjhxjk>
2024-03-31 17:32:51 +1000

    d24.5

--- d18/users/forms.py
+++ d18/users/forms.py
@@ -1,7 +1,42 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, forms, UserCreationForm, UserChangeForm
 from d18.users.models import User
 
 class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'pessword']
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Ввндите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Ввндите фамилию'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Ввндите email'}))
+    password1 = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Ввндите пароль'}))
+    password2 = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Повторите пароль'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholer': 'Введите имя пользователя'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', ' password1', 'password2']
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4',}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', }))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', }))
+    image = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', }))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', }))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'image', 'username')
